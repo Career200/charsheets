5aadfc76139c7783e4499176e2a1b76ddac0fcd0
Zhukov Dmitry <zhukontime@gmail.com>
2024-06-02 16:33:30 +0400

    feat: remove swc, add babel with stage3 decorators support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import * as path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	esbuild: {
 		minifyIdentifiers: false,
 		keepNames: true,
+		target: "esnext",
 	},
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [
+					[
+						"@babel/plugin-proposal-decorators",
+						{
+							version: "2023-11",
+						},
+					],
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@avatar": path.resolve(__dirname, "public/avatar"),
